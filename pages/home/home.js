228492d11dce66e6649a70a2f1d841a6aa961d86
DomFan <fanfansuger@126.com>
2018-03-29 17:51:35 +0800

    add foodlist to the home-page

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,7 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    item: {
+      "name": "咖啡小食随心配套餐",
+      "price": "12.0",
+      "url": "http://imgm.4008823823.com.cn/kfcmwos/img//S/270_117807.jpg"
+    },
+    imgArray: [],
+    // 商品列表
+    foodArray: [],
   },
 
   torder: function () {
@@ -19,7 +26,6 @@ Page({
     wx.navigateTo({
       url: '../order/order',
     })
-
     // {
     //   "selectedIconPath": "images/order_s.png",
     //     "iconPath": "images/order.png",
@@ -27,12 +33,56 @@ Page({
     //         "text": "订单"
     // },
   },
-
+  setstorage: function () {
+    // wx.getStorage({
+    //   key: 'aaa',
+    //   success: function (res) {
+    //     console.log('getStorage', res)
+    //   },
+    //   fail: function (res) { },
+    //   complete: function (res) { },
+    // })
+    
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    // wx.setStorage({
+    //   key: 'aaa',
+    //   data: 'hello',
+    //   success: function (res) {
+    //     console.log('setStorage', res)
+    //   },
+    //   fail: function (res) { },
+    //   complete: function (res) { },
+    // })
+
+    let that = this
+    wx.showToast({
+      title: '加载中',
+      icon: 'loading',
+      duration: 0,
+      mask: true
+    })
+    // 发送请求
+    wx.request({
+      url: 'http://easy-mock.com/mock/5905d4597a878d73716e2c6b/kfc/kfc',
+      method: 'GET',
+      data: {},
+      header: {
+        'Accept': 'application/json'
+      },
+      success: function (res) {
+        console.log(res.data)
+        that.setData({
+          imgArray: res.data.navArray,
+          foodArray: res.data.foodArray
+        })
+      }
+    }, function () {
+      wx.hideToast();
+    })
   },
 
   /**
